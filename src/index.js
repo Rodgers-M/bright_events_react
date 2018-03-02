10cd912b0d6ac3ff52c32f370566ad3c1e0f42ef
Rodgers-M <mkishagha@gmail.com>
2018-03-02 09:14:06 +0300

    add the main js file

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap3/dist/css/bootstrap.css'
+import './stylesheets/index.css';
+import Landing from './components/landing'
+
+ReactDOM.render(<Landing />, document.getElementById('root'));
