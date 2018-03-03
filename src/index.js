dce0688bd61d5786ab10b00797f2742aaf7594d3
Rodgers-M <mkishagha@gmail.com>
2018-03-04 00:01:16 +0300

    (feat) : add the app component and routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import 'bootstrap3/dist/css/bootstrap.css'
 import './stylesheets/index.css';
-import Landing from './components/landing'
+import routes from './components/routes'
 
-ReactDOM.render(<Landing />, document.getElementById('root'));
+ReactDOM.render(
+   <Router>
+    {routes}
+   </Router>, 
+    document.getElementById('root'));
