3e7aefd91c0e80653eb404748d9041b4fd22e5a8
Rodgers-M <mkishagha@gmail.com>
2018-03-13 08:41:12 +0300

    (feat) : modify the imports to match the new folder structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import './stylesheets/index.css';
-import routes from './components/routes'
+import App from './app';   
+import registerServiceWorker from './registerServiceWorker'; 
 
 ReactDOM.render(
-   <Router>
-    {routes}
-   </Router>, 
-    document.getElementById('root'));
+    <Router>
+        <App />        
+    </Router>,             
+    document.getElementById('root') 
+);
+registerServiceWorker();
