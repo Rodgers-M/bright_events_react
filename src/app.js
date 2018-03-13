a2aefcc2db458dae5026065370c7b4cd38e69e16
Rodgers-M <mkishagha@gmail.com>
2018-03-13 23:19:03 +0300

    (feat) : add a route to the login page

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route } from 'react-router-dom';
 import Landing from './components/pages/landing'
+import LoginPage from './components/pages/LoginPage'
 
 const App = () => <div className='ui'>
         <Route path='/' exact component={Landing} />
+        <Route path='/auth/login' exact component={LoginPage} />
     </div>
 
 export default App;
