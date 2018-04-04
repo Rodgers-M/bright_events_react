1dc8444c1469557ec41004776531c36c892d0891
Rodgers-M <mkishagha@gmail.com>
2018-04-04 17:18:22 +0300

    [feat] : add more data validation to the forms

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -11,8 +11,9 @@ class SignupForm extends Component {
             email : '',
             password : '',
             cnfpassword : '',
-            loading : false
-        }
+        },
+        loading : false,
+        errors : {}
     }
 
     onChange = e => this.setState({
@@ -22,8 +23,8 @@ class SignupForm extends Component {
     onSubmit = () => {
         const errors = this.validate(this.state.data);
         this.setState({ errors });
-        this.setState({loading : true});
         if(Object.keys(errors).length===0){
+            this.setState({loading : true});
             this.props.submit(this.state.data)
             .catch(err=> this.setState({errors : err.response.data, loading : false }));
         }
@@ -32,46 +33,55 @@ class SignupForm extends Component {
     validate=(data) => {
         const errors = {};
         if(Validator.isEmpty(data.username)) errors.username = 'username can\'t be blank';
-        if(!Validator.isEmail(data.email) || Validator.isEmpty(data.email)) errors.email = 'please provide a valid email';
+        if(!Validator.isEmail(data.email) || Validator.isEmpty(data.email)) errors.email
+            = 'please provide a valid email';
         if(Validator.isEmpty(data.password)) errors.password = 'Password can\'t be blank';
+        if(Validator.isLength(data.password, {min : 6, max : 16})) errors.password 
+            = 'Password must be more than 6 characters';
+        if(!Validator.equals(data.password , data.cnfpassword)) errors.password
+            = 'passwords do not match'
         return errors;
     }
 
     render(){
-        const { data} = this.state;
+        const { data, errors, loading} = this.state;
         return(
-            <Form onSubmit={this.onSubmit}>
-                <Form.Field >
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                <Form.Field error={!!errors.username} >
                     <label htmlFor='username' > username </label>
                     <input type='text' name='username' id='username'
                         placeholder='username'
                         value={data.username}
                         onChange={this.onChange}
                     />
+                {errors.username && <InLineError message={errors.username} /> }
                 </Form.Field>
-                <Form.Field >
+                <Form.Field error={!!errors.email} >
                     <label htmlFor='email' > Email </label>
                     <input type='email' name='email' id='email'
                         placeholder='example@example.com'
                         value={data.email}
                         onChange={this.onChange}
                     />
+                {errors.email && <InLineError message={errors.email} /> }
                 </Form.Field>
-                <Form.Field >
+                <Form.Field error={!!errors.password} >
                     <label htmlFor='password' > password </label>
                     <input type='password' name='password' id='password'
                         placeholder='Password'
                         value={data.password}
                         onChange={this.onChange}
                     />
+                {errors.password && <InLineError message={errors.password} /> }
                 </Form.Field>
-                <Form.Field >
+                <Form.Field error={!!errors.password}>
                     <label htmlFor='cnfpassword' > confirm password </label>
                     <input type='password' name='cnfpassword' id='cnfpassword'
                         placeholder='Password'
                         value={data.cnfpassword}
                         onChange={this.onChange}
-                    />
+                    /> 
+                {errors.password && <InLineError message={errors.password} /> }
                 </Form.Field>
             <Button primary> Signup </Button>
             </Form>
