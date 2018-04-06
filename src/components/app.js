51efeca14aa6c8d9e6ec86b24bb0979b4972e08d
Rodgers-M <mkishagha@gmail.com>
2018-04-06 06:53:42 +0300

    [feat] : add a component to display flash messages

--- src/components/app.js
+++ src/components/app.js
@@ -3,8 +3,10 @@ import { Route } from 'react-router-dom';
 import Landing from './pages/landing';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import FlashMessagesList from './messages/flasshMessagesList';
 
 const App = () => <div className='ui'>
+        <FlashMessagesList />
         <Route path='/' exact component={Landing} />
         <Route path='/auth/login' exact component={LoginPage} />
         <Route path='/auth/signup' exact component={SignupPage} />
