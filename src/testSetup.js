b0da241a2bd9a52cb7a24ec3f5830c29f5f77c54
Rodgers-M <mkishagha@gmail.com>
2018-04-06 07:13:24 +0300

    [feat] : define the test configurations

---
+++ src/testSetup.js
@@ -0,0 +1,19 @@
+process.env.NODE_ENV = 'test'
+require('babel-register')()
+// Disable webpack-specific features for tests since
+// Mocha doesn't know what to do with them.
+require.extensions['.css'] = function () {return null}
+// Configure JSDOM and set global variables
+// to simulate a browser environment for tests.
+var jsdom = require('jsdom').jsdom
+var exposedProperties = ['window', 'navigator', 'document']
+global.document = jsdom('')
+global.navigator = { userAgent: 'node.js' }
+global.window = document.defaultView
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property)
+    global[property] = document.defaultView[property]
+  }
+})
+documentRef = document
