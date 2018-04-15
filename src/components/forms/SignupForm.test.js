7417d324c2de038f6a787f5c59418d33f82b1a47
Rodgers-M <mkishagha@gmail.com>
2018-04-15 10:53:09 +0300

    add tets for the login form component

---
+++ src/components/forms/SignupForm.test.js
@@ -0,0 +1,38 @@
+import 'jsdom-global/register';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
+import expect from 'expect';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
+import {mount, shallow} from 'enzyme';
+import SignupForm from './SignupForm';
+    
+ 
+Enzyme.configure({ adapter: new Adapter() });
+
+function setup(){
+    let props = {
+        submit : () => {}
+    };
+    return mount(
+        <SignupForm {...props} />
+    );
+};
+
+describe('SignupForm form component', () => {
+    it('should have four input fields', ()=> {
+        const wrapper = setup();
+        expect(wrapper.find('input').length).toEqual(4);
+    });
+    it('should have one submit  button', ()=> {
+        const wrapper = setup();
+        expect(wrapper.find('button').length).toEqual(1);
+    });
+    it('should have no data in state', ()=> {
+        const wrapper = setup()  
+        const usernameInput = wrapper.find('#username');
+        usernameInput.instance().value = "myname";
+        usernameInput.simulate('change');
+        expect(wrapper.instance().state.data.username).toEqual('myname')
+    });
+});
