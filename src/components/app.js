f6e1640037f5af1c1094dcdabe69ff66d666296b
Rodgers-M <mkishagha@gmail.com>
2018-04-17 16:14:39 +0300

    [feat-155927917] : redirect to 404 on astray paths

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Landing from './pages/landing';
 import LoginPage from './pages/auth/LoginPage';
 import SignupPage from './pages/auth/SignupPage';
+import NotFound from './pages/NotFound';
 import FlashMessagesList from './messages/flasshMessagesList';
 
 const App = () => <div className='ui'>
         <FlashMessagesList />
-        <Route path='/' exact component={Landing} />
-        <Route path='/auth/login' exact component={LoginPage} />
-        <Route path='/auth/signup' exact component={SignupPage} />
+        <Switch>
+            <Route path='/' exact component={Landing} />
+            <Route path='/auth/login' exact component={LoginPage} />
+            <Route path='/auth/signup' exact component={SignupPage} />
+            <Route component={NotFound} />
+        </Switch>
     </div>
 
 export default App;
