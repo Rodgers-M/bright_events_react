4dedd7a72b5cc41c171c57e78792bbc35302643c
Rodgers-M <mkishagha@gmail.com>
2018-04-20 13:16:39 +0300

    [feat] : move rootreducer to reducers folder

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import {combineReducers} from 'redux';
-import user from './reducers/user';
-import flashMessages from './reducers/flashMessages';
+import user from './user';
+import flashMessages from './flashMessages';
 
 export default combineReducers({
     user,
