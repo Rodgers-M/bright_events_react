57165d62ca5c5e41153b3cdd0be41f92a83a7f5b
Rodgers-M <mkishagha@gmail.com>
2018-04-25 10:43:38 +0300

    [feat-155927917] : add provider and store to global variables

--- tests/helpers.js
+++ tests/helpers.js
@@ -5,7 +5,11 @@ import expect from 'expect';
 import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 import {mount, shallow} from 'enzyme';
+import configureStore from '../src/redux/store/configureStore';
+import {Provider} from 'react-redux';
 
+const store = configureStore();
+ 
 global.React = React;
 global.Router = Router;
 global.expect = expect;
@@ -13,4 +17,6 @@ global.Adapter = Adapter;
 global.Enzyme = Enzyme;
 global.mount = mount;
 global.shallow = shallow;
+global.store = store;
+global.Provider = Provider;
 
