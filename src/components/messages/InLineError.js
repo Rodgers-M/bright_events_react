34cb8f96df6ff20d8bf3cd020e8ded4fa1ff96f4
Rodgers-M <mkishagha@gmail.com>
2018-04-25 10:29:15 +0300

    [feat-155927917] : make the messages color more visible

--- src/components/messages/InLineError.js
+++ src/components/messages/InLineError.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import  PropTypes from 'prop-types';
 
-const InLineError = ({ message }) => <span style={{color: '#ae5856'}}>{ message } </span>;
+const InLineError = ({ message }) => <span style={{color: '#ae5856a'}}>{ message } </span>;
 
 InLineError.propTypes = {
     message: PropTypes.string.isRequired
