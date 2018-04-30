fe96b1f96a076585f3dacabf560c28ba2b848caa
Rodgers-M <mkishagha@gmail.com>
2018-04-30 15:28:31 +0300

    test the component onClick method

--- tests/messages/flashMessage.test.js
+++ tests/messages/flashMessage.test.js
@@ -9,20 +9,31 @@ const successMessage = {
 }; 
 const errorMessage = {
     type : 'error',
-    text : 'your action was successful, proceed'
+    text : 'your action failed, please try again later'
 }; 
 
 function setup(message){
     return mount(
-        <FlashMessage message={message} deleteFlashMessage={deleteFlashMessage} />,
-        options.get()
+        <FlashMessage message={message} deleteFlashMessage={deleteFlashMessage} />
     );
 };
  
 describe('Flash messages component', ()=> {
-   it('it should have one div', ()=> {
+   it('should have one div', ()=> {
        const wrapper = setup(successMessage)
        expect(wrapper.find('div').length).toEqual(1);
    });
-
+   it('should display the passed in message', ()=> {
+       const wrapper = setup(successMessage)
+       expect(wrapper.find('p').text()).toBe('your action was successful, proceed');
+   });
+   it('should call deleteFlashMessage when dismissed', ()=> {
+       const wrapper = setup(successMessage)
+       wrapper.find('i').simulate('click');
+       expect(deleteFlashMessage.called).toBe(true);
+   });
+   it('should have a div with class error on error message', ()=> {
+       const wrapper = setup(errorMessage)
+       expect(wrapper.find('.error').length).toBe(1);
+   });
 });
