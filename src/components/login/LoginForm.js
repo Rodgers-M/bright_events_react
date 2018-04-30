c90e862936b0e978399ee7a2573755581e36349d
Rodgers-M <mkishagha@gmail.com>
2018-04-30 15:18:05 +0300

    correct a typo in flassmessafes import

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -3,7 +3,7 @@ import {Form, Button, Message } from 'semantic-ui-react';
 import  PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import InLineError from '../messages/InLineError';
-import FlashMessagesList from '../messages/flasshMessagesList';
+import FlashMessagesList from '../messages/FlashMessagesList';
 
 var formInputStyle = {
     color : 'white',
