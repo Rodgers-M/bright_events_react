875101b1942bf028e5437625456a9819379fe17f
Rodgers-M <mkishagha@gmail.com>
2018-04-30 17:09:00 +0300

    test it displays a list of messages

---
+++ tests/messages/flashMessagesList.test.js
@@ -0,0 +1,28 @@
+import '../helpers'
+import {FlashMessagesList} from '../../src/components/messages/FlashMessagesList';
+
+const messages = [
+    {id : '1', type : 'error', text : 'your action failed, please try again later'},
+    {id : '2', type : 'sucess', text : 'your action was successful, proceed'},
+    {id : '3', type : 'error', text : 'your action failed, please try again later'},
+] 
+ 
+const deleteFlashMessage = sinon.spy();
+
+function setup(){
+    return mount(
+        <FlashMessagesList messages={messages} deleteFlashMessage={deleteFlashMessage} />
+    );
+};
+
+describe('FlashMessagesList component', ()=> {
+   it('should have four  div elements when displaying 3 messages', ()=> {
+       const wrapper = setup()
+       expect(wrapper.find('div').length).toEqual(4);
+   });
+   it('should have two  div elements with error class', ()=> {
+       const wrapper = setup()
+       expect(wrapper.find('.error').length).toEqual(2);
+   });
+});
+
