ec8ccad3978e304727552aec8c69ecb0b9c4950e
Rodgers-M <mkishagha@gmail.com>
2018-05-06 14:36:01 +0300

    [feat] : add routes to create and view events

--- src/components/app.js
+++ src/components/app.js
@@ -3,15 +3,21 @@ import { Route, Switch } from 'react-router-dom';
 import Landing from './home/landing';
 import LoginContainer from './login/LoginContainer';
 import SignupContainer from './signup/SignupContainer';
+import CreateEventContainer from './events/CreateEventContainer';
 import NotFound from './home/NotFound';
+import EventsLayout from './events/EventsLayout';
+import EventList from './events/EventList';
 
 const App = () => <div className='ui'>
         <Switch>
             <Route path='/' exact component={Landing} />
             <Route path='/auth/login' exact component={LoginContainer} />
             <Route path='/auth/signup' exact component={SignupContainer} />
+            <EventsLayout path='/events/create' exact component={CreateEventContainer} page='create' />
+            <EventsLayout path='/events/myEvents' exact component={EventList} page='myEvents' />
             <Route component={NotFound} />
         </Switch>
     </div>
 
 export default App;
+
