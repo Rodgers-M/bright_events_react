3fca7659db53f5a420df7ff9b974139dce4272f6
Rodgers-M <mkishagha@gmail.com>
2018-05-08 23:49:07 +0300

    [feat] : return null if token is unavailable

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,17 +1,17 @@
 import jwtDecode from 'jwt-decode';
  
 export const isTokenExpired = token => {
-    let currentTime = new Date() / 1000;
+    const currentTime = new Date() / 1000;
     if(jwtDecode(token).exp < currentTime){
         localStorage.removeItem('brighteventsJWT');
         return true;
-    }else{
-        return false;
     }
-}
+    return false;
+};
 
 export const getUsername = token => {
     if(token){
         return jwtDecode(token).username;
     }
-}
+    return null;
+};
