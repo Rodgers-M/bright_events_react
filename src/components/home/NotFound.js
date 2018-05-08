9e002252d5d55a0fa7e463dd49670c8f44765776
Rodgers-M <mkishagha@gmail.com>
2018-05-08 23:43:29 +0300

    [feat] : convert notfound into functional component

--- src/components/home/NotFound.js
+++ src/components/home/NotFound.js
@@ -1,34 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../stylesheets/notfound.css';
 
-class NotFound extends Component {
-
-    render(){
-        return(
-            <div className='notfound'>
-                    <div className="circles">
-                      <p>404</p> 
-                      <h1>Whooops, seems you lost your way, lets get back</h1> 
-                        <h1>
-                             <Link to='/'>
-                               <i className='home icon' /> Home 
-                            </Link>
-                        </h1>
-                       <Link to='/'>
-                      <span className="circle med"></span>
-                       </Link>
-                       <Link to='/'>
-                      <span className="circle small"></span>
-                       </Link>
-                       <Link to='/'>
-                      <span className="circle big"></span>
-                       </Link>
-                    </div>
-            </div>
-        );
-    }
-    
-}
+const  NotFound = () =>(
+    <div className='notfound'>
+        <div className="circles">
+            <p>404</p> 
+            <h1>Whooops, seems you lost your way, lets get back</h1> 
+            <h1>
+                <Link to='/'>
+                    <i className='home icon' /> Home 
+                </Link>
+            </h1>
+            <Link to='/'>
+                <span className="circle med" />
+            </Link>
+            <Link to='/'>
+                <span className="circle small" />
+            </Link>
+            <Link to='/'>
+                <span className="circle big" />
+            </Link>
+        </div>
+    </div>
+);
 
 export default NotFound;
