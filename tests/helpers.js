7b5824790155804f38451352c2559b2c2677c39b
Rodgers-M <mkishagha@gmail.com>
2018-05-09 17:32:22 +0300

    [test] : remove react router replace the router use with options.get from react-router-enzyme context

--- tests/helpers.js
+++ tests/helpers.js
@@ -1,20 +1,17 @@
 import 'jsdom-global/register';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
 import expect from 'expect';
 import Adapter from 'enzyme-adapter-react-16';
-import Enzyme from 'enzyme';
-import {mount, shallow} from 'enzyme';
-import configureStore from '../src/redux/store/configureStore';
+import Enzyme,{mount, shallow} from 'enzyme';
 import {Provider} from 'react-redux';
 import sinon from 'sinon';
 import ReactRouterEnzymeContext from 'react-router-enzyme-context';
+import configureStore from '../src/redux/store/configureStore';
 
 const store = configureStore();
 const options = new ReactRouterEnzymeContext();
  
 global.React = React;
-global.Router = Router;
 global.expect = expect;
 global.Adapter = Adapter;
 global.Enzyme = Enzyme;
