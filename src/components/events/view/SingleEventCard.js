291062418e9509b1a986c2a32ec425e029ebbd7f
Rodgers-M <mkishagha@gmail.com>
2018-05-16 23:46:28 +0300

    [feat] : format date to be more presentable

--- src/components/events/view/SingleEventCard.js
+++ src/components/events/view/SingleEventCard.js
@@ -2,17 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SingleEventCard = ({event, pathName}) => {
-    const {name, description, location} = event;
+    const {name, description, location, orgarniser} = event;
+    const eventDate = event.event_date.split('00')[0];
     return (
         <div className='ui card'>
             <div className='content'>
                 <div className='header'>{name}</div> 
             </div>
             <div className='content'>
-                <h4 className="ui sub header">Host {event.created_by}</h4>
+                <h4 className="ui sub header">Host : {orgarniser}</h4>
                 <div className='meta'>
-                    <span>Location {location}</span> 
-                    <span>Date {event.event_date}</span> 
+                    <span>Location : {location}</span> <br/>
+                    <span>Date : {eventDate}</span> 
                 </div>
                 <div className='description'>
                     <p>{description}</p>
