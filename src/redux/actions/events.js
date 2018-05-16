a20605ad3ca0d9292258178dd271a821b1142101
Rodgers-M <mkishagha@gmail.com>
2018-05-16 23:45:27 +0300

    [feat] : define fetch personal events action

--- src/redux/actions/events.js
+++ src/redux/actions/events.js
@@ -1,4 +1,4 @@
-import {EVENT_CREATED, EVENTS_FETCHED} from './types';
+import {EVENT_CREATED, ALL_EVENTS_FETCHED, MY_EVENTS_FETCHED} from './types';
 import api from '../../api';
 
 
@@ -8,16 +8,26 @@ export const eventCreated = (createdEvent) => ({
 });
 
 export const eventsFetched = (events) => ({
-    type : EVENTS_FETCHED,
+    type : ALL_EVENTS_FETCHED,
     events  
 });
  
+export const myEventsFetched = (events) => ({
+    type : MY_EVENTS_FETCHED,
+    events  
+});
+
 export const create = details => (dispatch) => 
     api.events.create(details).then(event =>{
         dispatch(eventCreated(event));
     });
 
 export const fetchEvents = () => (dispatch) => 
-    api.events.fetch().then(events =>{
+    api.events.fetchAll().then(events =>{
         dispatch(eventsFetched(events));
+    });
+
+export const fetchMyEvents = () => (dispatch) => 
+    api.events.fetchMyEvents().then(events =>{
+        dispatch(myEventsFetched(events));
     });
