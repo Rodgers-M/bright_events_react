e9acee659f68362c87986ea9515b52b6e7d63be2
Rodgers-M <mkishagha@gmail.com>
2018-05-16 13:33:31 +0300

    [feat] : change the events components path to new folders

--- src/components/app.js
+++ src/components/app.js
@@ -3,10 +3,10 @@ import { Route, Switch } from 'react-router-dom';
 import Landing from './home/landing';
 import LoginContainer from './login/LoginContainer';
 import SignupContainer from './signup/SignupContainer';
-import CreateEventContainer from './events/CreateEventContainer';
+import CreateEventContainer from './events/create/CreateEventContainer';
 import NotFound from './home/NotFound';
 import EventsLayout from './events/EventsLayout';
-import EventList from './events/EventList';
+import EventList from './events/view/EventList';
 
 const App = () => <div className='ui'>
     <Switch>
