f01a4c5c02c474caf4a263be13c7d157e0429514
Rodgers-M <mkishagha@gmail.com>
2018-05-21 22:52:22 +0300

    [test] : add tests for the async event actions

--- tests/redux/actions/events.test.js
+++ tests/redux/actions/events.test.js
@@ -1,17 +1,16 @@
 /* global describe :true */
 /* global it :true */
-/* global expect afterEach :true */
+/* global expect afterEach beforeEach:true */
 /* eslint no-undef: "error" */
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import moxios from 'moxios';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as actions from '../../../src/redux/actions/events';
 import * as types from '../../../src/redux/actions/types';
+import {instance} from '../../../src/api';
 
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
-const mock = new MockAdapter(axios);
 
 describe('Events Actions', ()=> {
     describe('Event created action creator', ()=> {
@@ -45,15 +44,61 @@ describe('Events Actions', ()=> {
         });
     });
     describe('Async actions', ()=> {
-        afterEach(()=> {
-            mock.reset();
+        beforeEach(()=> {
+            moxios.install(instance);
         });
-        it.skip('should dispacth ALL_EVENTS_FETCHED on success', ()=> {
-            mock.onGet('/events/all').reply(200, [{name : 'my event'}]);
-            const expectedAction = {type: actions.ALL_EVENTS_FETCHED, events: [{name: 'my event'}]}; 
-            const store = mockStore({ events: [] });
-            return store.dispatch(actions.fetchEvents()).then(()=> {
-                expect(store.getActions()).toEqual(expectedAction);
+        afterEach(()=> {
+            moxios.install(instance);
+        });
+        describe('ALL_EVENTS_FETCHED action', ()=> {
+            it('should dispacth ALL_EVENTS_FETCHED on success', ()=> {
+                const events = [{name: 'all events'}];
+                moxios.wait(()=> {
+                    const request = moxios.requests.mostRecent();
+                    request.respondWith({
+                        status : 200,
+                        response : events
+                    });
+                });
+                const expectedAction = [{type: types.ALL_EVENTS_FETCHED, events: [{name: 'all events'}]}]; 
+                const store = mockStore({ events: [] });
+                return store.dispatch(actions.fetchEvents()).then(()=> {
+                    expect(store.getActions()).toEqual(expectedAction);
+                });
+            });
+        });
+        describe('MY_EVENTS_FETCHED action', ()=> {
+            it('should dispatch MY_EVENTS_FETCHED on success', ()=> {
+                const events = [{name: 'my event'}];
+                moxios.wait(()=> {
+                    const request = moxios.requests.mostRecent();
+                    request.respondWith({
+                        status : 200,
+                        response : events
+                    });
+                });
+                const expectedAction = [{type: types.MY_EVENTS_FETCHED, events: [{name: 'my event'}]}]; 
+                const store = mockStore({ events: [] });
+                return store.dispatch(actions.fetchMyEvents()).then(()=> {
+                    expect(store.getActions()).toEqual(expectedAction);
+                });
+            });
+        });
+        describe('EVENT_CREATED action', ()=> {
+            it('should dispatch EVENT_CREATED on success', ()=> {
+                const createdEvent = {name: 'new event'};
+                moxios.wait(()=> {
+                    const request = moxios.requests.mostRecent();
+                    request.respondWith({
+                        status : 201,
+                        response : createdEvent
+                    });
+                });
+                const expectedAction = [{type: types.EVENT_CREATED, createdEvent: {name: 'new event'}}]; 
+                const store = mockStore({ events: [] });
+                return store.dispatch(actions.create()).then(()=> {
+                    expect(store.getActions()).toEqual(expectedAction);
+                });
             });
         });
     });
