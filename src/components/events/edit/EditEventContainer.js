849c9866351085d341de35fea49bfd1f670327d8
Rodgers-M <mkishagha@gmail.com>
2018-05-26 12:07:24 +0300

    [feat] : add edit event components

---
+++ src/components/events/edit/EditEventContainer.js
@@ -0,0 +1,103 @@
+/* eslint new-cap: ["error", { "newIsCap": false }] */
+/* eslint-disable react/no-unused-state */
+import React, {Component} from 'react';
+import  PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import CreateEventForm from '../EventForm';
+import { updateEvent } from '../../../redux/actions/events';
+import { formatDate, validateEventData } from '../../helpers/helpers';
+
+export class EditEventContainer extends Component{
+    state = {
+        data: { 
+            id : '',
+            name: '',
+            description: '',
+            category: '',
+            location: '',
+            event_date: moment() 
+        },
+        loading: false,
+        errors: {}
+    };
+
+
+    componentWillMount(){
+        const events = this.props.events;
+        const filteredEvent = events.filter(event=>event.id == this.props.match.params.id);
+        const event = filteredEvent[0];
+        this.setState({
+            data : {
+                ...this.state.data, id: event.id, name: event.name, category: event.category,
+                description: event.description, location : event.location
+            } 
+        });
+    }
+
+    onChange = e => this.setState({
+        data : {...this.state.data, [e.target.name]: e.target.value}
+    });
+
+
+    onSubmit = () => {
+        const errors = validateEventData(this.state.data);
+        this.setState({ errors });
+        if(Object.keys(errors).length===0){
+            this.setState({loading : true});
+            const eventDetails = {...this.state.data, event_date : formatDate(this.state.data.event_date)};
+            this.props.updateEvent(eventDetails, this.state.data.id)
+                .then(() =>{
+                    this.setState({loading : false});
+                    this.props.history.push('/events/myEvents');
+                })
+                .catch(err => {
+                    if (err.request.status === 500){ 
+                        this.setState({errors: {message: 'Service is unavailable, please try again later'},loading : false});
+                    }else {
+                        this.setState({errors: err.response.data, loading : false});
+                    }
+                });
+        }
+    };
+
+    handleDismiss= () => {
+        this.setState({
+            errors : {} 
+        });
+    }
+    handleDate = date => this.setState({
+        data : {...this.state.data, event_date : date }
+    });
+
+    render(){
+        return(
+            <div >
+                <CreateEventForm
+                    onSubmit={this.onSubmit} 
+                    onChange={this.onChange}
+                    handleDate={this.handleDate}
+                    handleDismiss={this.handleDismiss}
+                    state={this.state}
+                    buttonText='Update'
+                />
+            </div>
+        );
+    }
+}
+
+
+
+function mapStateToProps(state) {
+    return {
+        events :  state.events
+    };
+}
+
+EditEventContainer.propTypes = {
+    events: PropTypes.array.isRequired,
+    updateEvent: PropTypes.func.isRequired
+};
+
+export default connect(mapStateToProps,{ updateEvent } )(EditEventContainer);
+
---
+++ src/components/events/edit/EditModal.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+import EditEventContainer from './EditEventContainer';
+
+
+export class EditModal extends Component {
+
+    render() {
+        return (
+            <div className='custom-modal'>
+
+            </div>
+        );
+    }
+}
+
+export default EditModal;
+
