89e3b3bdeb4611faf7629c238e6a4f5f1a2c8fc6
Rodgers-M <mkishagha@gmail.com>
2018-05-26 11:41:39 +0300

    [feat] : add a route to edit events page

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import Landing from './home/landing';
 import LoginContainer from './login/LoginContainer';
 import SignupContainer from './signup/SignupContainer';
 import CreateEventContainer from './events/create/CreateEventContainer';
+import EditEventContainer from './events/edit/EditEventContainer';
 import NotFound from './home/NotFound';
 import EventsLayout from './events/EventsLayout';
 import EventList from './events/view/EventList';
@@ -16,6 +17,7 @@ const App = () => <div className='ui'>
         <Route path='/auth/signup' exact component={SignupContainer} />
         <EventsLayout path='/events/create' exact component={CreateEventContainer} page='create' />
         <EventsLayout path='/events/myEvents' exact component={MyEvents} page='myEvents' />
+        <EventsLayout path='/events/:id/edit' exact component={EditEventContainer} page='edit' />
         <EventsLayout path='/events' exact component={EventList} page='allEvents' />
         <Route component={NotFound} />
     </Switch>
