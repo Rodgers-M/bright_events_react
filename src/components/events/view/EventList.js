3838beba39f21fb0586eb8d648bcccdbf9c44819
Rodgers-M <mkishagha@gmail.com>
2018-05-29 09:44:40 +0300

    [feat] : define rsvp handler

--- src/components/events/view/EventList.js
+++ src/components/events/view/EventList.js
@@ -3,7 +3,7 @@ import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SingleEventCard  from './SingleEventCard';
-import { fetchEvents } from '../../../redux/actions/events';
+import { fetchEvents, rsvp } from '../../../redux/actions/events';
 
 class EventList extends Component {
 
@@ -18,6 +18,7 @@ class EventList extends Component {
             <div className='ui three cards'>
                 {this.props.allEvents.map(event =>
                     <SingleEventCard
+                        rsvp={this.props.rsvp}
                         event={event}
                         pathName={this.props.location.pathname}
                         key={event.id}
@@ -36,9 +37,10 @@ function mapStateToProps(state) {
 EventList.propTypes = {
     allEvents: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
     fetchEvents: PropTypes.func.isRequired,
+    rsvp: PropTypes.func.isRequired,
     location: PropTypes.shape({
         pathname : PropTypes.string.isRequired
     }).isRequired
 };
 
-export default connect(mapStateToProps, {fetchEvents})(EventList);
+export default connect(mapStateToProps, { fetchEvents, rsvp })(EventList);
