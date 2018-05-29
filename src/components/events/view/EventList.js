ea11926114c2a8eb5d6e9b03f8de2f7547b7741c
Rodgers-M <mkishagha@gmail.com>
2018-05-29 16:11:34 +0300

    [feat] : toggle the content at the footer depending on user and rsvp status

--- src/components/events/view/EventList.js
+++ src/components/events/view/EventList.js
@@ -2,6 +2,7 @@
 import React, { Component} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import {addFlashMessage} from '../../../redux/actions/flashMessages';
 import SingleEventCard  from './SingleEventCard';
 import { fetchEvents, rsvp } from '../../../redux/actions/events';
 
@@ -13,6 +14,11 @@ class EventList extends Component {
 
     callFetchEvents = () => this.props.fetchEvents();
 
+    handleErrors = () => this.props.addFlashMessage({
+        type : 'warning',
+        text : 'you already reserved a seat' 
+    }) 
+
     render() {
         return (
             <div className='ui three cards'>
@@ -22,6 +28,9 @@ class EventList extends Component {
                         event={event}
                         pathName={this.props.location.pathname}
                         key={event.id}
+                        dismissMessage={this.handleDismiss}
+                        handleErrors={this.handleErrors}
+                        username={this.props.username}
                     />
                 )}
             </div>
@@ -37,10 +46,11 @@ function mapStateToProps(state) {
 EventList.propTypes = {
     allEvents: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
     fetchEvents: PropTypes.func.isRequired,
+    addFlashMessage: PropTypes.func.isRequired,
     rsvp: PropTypes.func.isRequired,
     location: PropTypes.shape({
         pathname : PropTypes.string.isRequired
     }).isRequired
 };
 
-export default connect(mapStateToProps, { fetchEvents, rsvp })(EventList);
+export default connect(mapStateToProps, { fetchEvents, rsvp, addFlashMessage })(EventList);
--- src/components/events/view/SingleEventCard.js
+++ src/components/events/view/SingleEventCard.js
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const SingleEventCard = ({event, pathName, onDelete, rsvp}) => {
-    const {name, description, location, orgarniser} = event;
-    const eventDate = event.event_date.split('00')[0];
+const SingleEventCard = (props) => {
+    const { event, pathName, onDelete, rsvp, handleErrors, username } = props;
+    const { name, description, location, orgarniser } = event;
     const url = `/events/${event.id}/edit`;
+
+    const eventDate = event.event_date.split('00')[0];
     const handleDelete = () => {
         onDelete(event.id);
     };
+    const host = orgarniser === username ? 'Me' : orgarniser;
     const handleRsvp = () => {
-        rsvp(event.id);
+        rsvp(event.id).catch(()=> handleErrors());
     };
     return (
         <div className='ui card'>
@@ -18,7 +21,7 @@ const SingleEventCard = ({event, pathName, onDelete, rsvp}) => {
                 <div className='header' style={{textTransform: 'capitalize'}}>{name}</div> 
             </div>
             <div className='content'>
-                <h4 className="ui sub header">Host : {orgarniser}</h4>
+                <h4 className="ui sub header">Host : {host}</h4>
                 <div className='meta'>
                     <span>Location : {location}</span> <br/>
                     <span>Date : {eventDate}</span> 
@@ -39,10 +42,23 @@ const SingleEventCard = ({event, pathName, onDelete, rsvp}) => {
                 </div>
                 :
                 <div className='extra content' id='rsvp'>
-                    <button onClick={handleRsvp} className="ui teal basic button">
-                        <i className="calendar plus outline icon" />
-                        RSVP
-                    </button>
+                    {orgarniser === username ? 
+                        <p>Your are the Host of this event</p>
+                        :
+                        <span>
+                        {event.rsvp_list.includes(username)?
+                            <div className='ui two buttons'>
+                            <div className="ui green basic button">Attending</div>
+                            <button  className="ui brown basic button">Cancel RSVP</button>
+                            </div>
+                            :
+                            <button onClick={handleRsvp} className="ui teal basic button">
+                            <i className="calendar plus outline icon" />
+                            RSVP
+                            </button>
+                        }
+                        </span>
+                    }
                 </div>
             }
         </div>
@@ -51,10 +67,11 @@ const SingleEventCard = ({event, pathName, onDelete, rsvp}) => {
 
 
 SingleEventCard.propTypes = {
-    event : PropTypes.shape({}).isRequired,
-    pathName : PropTypes.string.isRequired,
-    onDelete : PropTypes.func,
-    rsvp : PropTypes.func
+    event: PropTypes.shape({}).isRequired,
+    pathName: PropTypes.string.isRequired,
+    onDelete: PropTypes.func,
+    handleErrors: PropTypes.func,
+    rsvp: PropTypes.func
 };
 
 export default SingleEventCard;
