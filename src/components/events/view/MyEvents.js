ae87390d30ebc8d56bff8f1ab078f84c75a190ca
Rodgers-M <mkishagha@gmail.com>
2018-06-01 23:33:44 +0300

    [feat] : change button to fire open modal

--- src/components/events/view/MyEvents.js
+++ src/components/events/view/MyEvents.js
@@ -4,6 +4,9 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import SingleEventCard  from './SingleEventCard';
 import {fetchMyEvents, onDelete} from '../../../redux/actions/events';
+import {openModal, closeModal} from '../../../redux/actions/modals';
+import EventsModal from '../EventsModal';
+import EditEventContainer from '../edit/EditEventContainer';
 
 class MyEvents extends Component {
 
@@ -13,17 +16,35 @@ class MyEvents extends Component {
 
     callFetchMyEvents = () => this.props.fetchMyEvents();
 
+
+    renderModal = () => {
+
+        if (this.props.modal.open){
+           const  filteredEvent = this.props.MyEvents.filter(event=>event.id == this.props.modal.id);
+            const event = filteredEvent[0];
+            console.log('here is the event', event);
+            return(
+                <EventsModal onClose={this.props.closeModal} >
+                    <EditEventContainer event={event} />
+                </EventsModal>
+            );
+        }
+        return null;
+    };
+
     render() {
         return (
-            <div className='ui three cards'>
+            <div className='ui three cards' style={{overFlow: 'hidden'}}>
                 {this.props.MyEvents.map(event =>
                     <SingleEventCard
                         event={event}
                         pathName={this.props.location.pathname}
                         onDelete={this.props.onDelete}
                         key={event.id}
+                        openModal={this.props.openModal}
                     />
                 )}
+                        {this.renderModal()}
             </div>
         );
     }
@@ -31,7 +52,8 @@ class MyEvents extends Component {
 
 function mapStateToProps(state) {
     return {
-        MyEvents :  state.events
+        MyEvents:  state.events,
+        modal: state.modal
     };
 }
 MyEvents.propTypes = {
@@ -42,4 +64,4 @@ MyEvents.propTypes = {
     }).isRequired
 };
 
-export default connect(mapStateToProps, {fetchMyEvents, onDelete})(MyEvents);
+export default connect(mapStateToProps, {fetchMyEvents, onDelete, openModal, closeModal})(MyEvents);
