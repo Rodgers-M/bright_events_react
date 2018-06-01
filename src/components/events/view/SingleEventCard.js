01a3397fc030d32ec87fddbf88656d6049633ef9
Rodgers-M <mkishagha@gmail.com>
2018-06-01 23:34:46 +0300

    [feat] : change button to fire open modal

--- src/components/events/view/SingleEventCard.js
+++ src/components/events/view/SingleEventCard.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
 const SingleEventCard = (props) => {
     const { event, pathName, onDelete, rsvp, handleErrors, username } = props;
     const { name, description, location, orgarniser } = event;
-    const url = `/events/${event.id}/edit`;
 
     const eventDate = event.event_date.split('00')[0];
     const handleDelete = () => {
@@ -15,6 +13,7 @@ const SingleEventCard = (props) => {
     const handleRsvp = () => {
         rsvp(event.id).catch(()=> handleErrors());
     };
+
     return (
         <div className='ui card'>
             <div className='content'>
@@ -33,10 +32,12 @@ const SingleEventCard = (props) => {
             {pathName === '/events/myEvents' ?
                 <div className='extra content'>
                     <div className='ui three buttons'>
-                        <button className="ui green basic button">Guests</button>
-                        <div className='ui basic teal button'>
-                            <Link className='ui' to={url}>Edit </Link>
-                        </div>
+                        <button className="ui green basic button">
+                            Guests
+                        </button>
+                        <button onClick={()=>props.openModal(event.id)} className="ui teal basic button">
+                            Edit
+                        </button>
                         <button onClick={handleDelete} className="ui red basic button">Delete</button>
                     </div>
                 </div>
@@ -46,17 +47,17 @@ const SingleEventCard = (props) => {
                         <p>Your are the Host of this event</p>
                         :
                         <span>
-                        {event.rsvp_list.includes(username)?
-                            <div className='ui two buttons'>
-                            <div className="ui green basic button">Attending</div>
-                            <button  className="ui brown basic button">Cancel RSVP</button>
-                            </div>
-                            :
-                            <button onClick={handleRsvp} className="ui teal basic button">
-                            <i className="calendar plus outline icon" />
-                            RSVP
-                            </button>
-                        }
+                            {event.rsvp_list.includes(username)?
+                                <div className='ui two buttons'>
+                                    <div className="ui green basic button">Attending</div>
+                                    <button  className="ui brown basic button">Cancel RSVP</button>
+                                </div>
+                                :
+                                <button onClick={handleRsvp} className="ui teal basic button">
+                                    <i className="calendar plus outline icon" />
+                                RSVP
+                                </button>
+                            }
                         </span>
                     }
                 </div>
