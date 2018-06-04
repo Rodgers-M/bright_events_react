3f8bc3c3dde97c4785e50f522d125e6a4f52e5f6
Rodgers-M <mkishagha@gmail.com>
2018-06-04 12:32:31 +0300

    [feat] : pass a delete rsvp prop

--- src/components/events/view/EventList.js
+++ src/components/events/view/EventList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import {addFlashMessage} from '../../../redux/actions/flashMessages';
 import SingleEventCard  from './SingleEventCard';
-import { fetchEvents, rsvp } from '../../../redux/actions/events';
+import { fetchEvents, rsvp, deleteRsvp } from '../../../redux/actions/events';
 import NoEventsMessage from '../NoEventsMessage';
 
 class EventList extends Component {
@@ -21,14 +21,15 @@ class EventList extends Component {
     }) 
 
     render() {
-        const{ allEvents, location, username } = this.props;
+        const{ allEvents, location, username, rsvp, deleteRsvp } = this.props;
         return (
             <div>
                 {allEvents.length !== 0 ?
                     <div className='ui three cards'>
                         {this.props.allEvents.map(event =>
                             <SingleEventCard
-                                rsvp={this.props.rsvp}
+                                rsvp={rsvp}
+                                deleteRsvp={deleteRsvp}
                                 event={event}
                                 pathName={location.pathname}
                                 key={event.id}
@@ -55,10 +56,11 @@ EventList.propTypes = {
     fetchEvents: PropTypes.func.isRequired,
     addFlashMessage: PropTypes.func.isRequired,
     rsvp: PropTypes.func.isRequired,
+    deleteRsvp: PropTypes.func.isRequired,
     location: PropTypes.shape({
         pathname : PropTypes.string.isRequired
     }).isRequired,
     username: PropTypes.string.isRequired
 };
 
-export default connect(mapStateToProps, { fetchEvents, rsvp, addFlashMessage })(EventList);
+export default connect(mapStateToProps, { fetchEvents, rsvp,deleteRsvp, addFlashMessage })(EventList);
