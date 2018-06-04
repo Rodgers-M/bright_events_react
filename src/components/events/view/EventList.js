60cca7b201c5343992e2f8287336ce60bc00775b
Rodgers-M <mkishagha@gmail.com>
2018-06-05 01:00:58 +0300

    [feat] : support event filter and serach

--- src/components/events/view/EventList.js
+++ src/components/events/view/EventList.js
@@ -6,8 +6,14 @@ import {addFlashMessage} from '../../../redux/actions/flashMessages';
 import SingleEventCard  from './SingleEventCard';
 import { fetchEvents, rsvp, deleteRsvp } from '../../../redux/actions/events';
 import NoEventsMessage from '../common/NoEventsMessage';
+import SearchComponent from '../common/SearchComponent';
+import NoResults from '../common/NoResults';
 
 class EventList extends Component {
+
+    state={
+        search: ''
+    }
 
     componentDidMount() {
         this.callFetchEvents();
@@ -20,26 +26,42 @@ class EventList extends Component {
         text : 'you already reserved a seat' 
     }) 
 
+    updateSearch = event =>{
+        this.setState({ search: event.target.value});
+        console.log(event.target.value);
+    }
+
     render() {
         const{ allEvents, location, username, rsvp, deleteRsvp } = this.props;
+        let filteredEvents = allEvents.filter(event => {
+            return event.name.toLowerCase().includes(this.state.search) ||
+               event.category.toLowerCase().includes(this.state.search) ||
+               event.location.toLowerCase().includes(this.state.search);
+        });
         return (
             <div>
-                {allEvents.length !== 0 ?
-                    <div className='ui three cards'>
-                        {this.props.allEvents.map(event =>
-                            <SingleEventCard
-                                rsvp={rsvp}
-                                deleteRsvp={deleteRsvp}
-                                event={event}
-                                pathName={location.pathname}
-                                key={event.id}
-                                dismissMessage={this.handleDismiss}
-                                handleErrors={this.handleErrors}
-                                username={username}
-                            />
-                        )}
-                    </div>
-                    : <NoEventsMessage username={username} pathName={location.pathname}/>
+                <SearchComponent onChange={this.updateSearch} />
+                {allEvents.length === 0?
+                    //show a message if no events exist in the app
+                    <NoEventsMessage username={username} pathName={location.pathname}/>
+                    :filteredEvents.length !== 0 ?
+                    //filteredEvents holds either all events or search results
+                        <div className='ui three cards'>
+                            {filteredEvents.map(event =>
+                                <SingleEventCard
+                                    rsvp={rsvp}
+                                    deleteRsvp={deleteRsvp}
+                                    event={event}
+                                    pathName={location.pathname}
+                                    key={event.id}
+                                    dismissMessage={this.handleDismiss}
+                                    handleErrors={this.handleErrors}
+                                    username={username}
+                                />
+                            )}
+                        </div>
+                        //if the search did not match any events
+                        : <NoResults /> 
                 }
             </div>
         );
