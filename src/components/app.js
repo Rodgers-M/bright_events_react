80b878553f4ee87b38d5bcd67b3dae611d10eeeb
Rodgers-M <mkishagha@gmail.com>
2018-06-18 11:36:56 +0300

    [feat] : add routes to forgot password components

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Landing from './home/landing';
-import LoginContainer from './login/LoginContainer';
-import SignupContainer from './signup/SignupContainer';
+import LoginContainer from './users/login/LoginContainer';
+import SignupContainer from './users/signup/SignupContainer';
+import ResetPasswordContainer from './users/resetPassword/ResetPasswordContainer';
 import CreateEventContainer from './events/create/CreateEventContainer';
 import EditEventContainer from './events/edit/EditEventContainer';
 import NotFound from './home/NotFound';
@@ -15,6 +16,8 @@ const App = () => <div className='ui'>
         <Route path='/' exact component={ Landing } />
         <Route path='/auth/login' exact component={ LoginContainer } />
         <Route path='/auth/signup' exact component={ SignupContainer } />
+        <Route path='/auth/forgortpass' exact component={ ResetPasswordContainer } />
+        <Route path='/auth/reset' exact component={ ResetPasswordContainer } />
         <EventsLayout path='/events/create' exact component={ CreateEventContainer } page='create' />
         <EventsLayout path='/events/myEvents' exact component={ MyEvents } page='myEvents' />
         <EventsLayout path='/events/:id/edit' exact component={ EditEventContainer } page='edit' />
